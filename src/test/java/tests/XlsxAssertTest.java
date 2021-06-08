ff869c4fa10f3f38e5efbd2dbff585110ce7e6ac
Rastro1994 <423@mail.ru>
2021-06-08 20:28:31 +0500

    DownloadTest

--- src/test/java/tests/XlsxAssertTest.java
+++ src/test/java/tests/XlsxAssertTest.java
@@ -8,7 +8,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static utils.Files.readXlsxFromPath;
 
-public class XlxxAssertTest {
+public class XlsxAssertTest {
     @Test
     void xlsxAssertTest() throws IOException {
         String xlsxFilePath = "./src/test/resources/files/1.xlsx";
